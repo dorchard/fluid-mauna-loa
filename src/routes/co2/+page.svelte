a31424d5a26891de6f779782203265c030472995
Dominic Orchard <dom.orchard@gmail.com>
2026-07-03 18:17:00 +0100

    add co2 route, fld files, data, and fix workflow BASE_PATH

---
+++ src/routes/co2/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { DataPane, Figure, Grid } from '@explorable-viz/fluid';
+
+	let showDataPane = false;
+
+	const spec = {
+		fluidSrcPath: ['../fluid'],
+		inputs: ["co2"],
+		query: false,
+		linking: false
+	};
+
+	const toggleDataPane = () => {
+		showDataPane = !showDataPane;
+	};
+</script>
+
+<Grid {showDataPane}>
+	<div></div>
+	<div></div>
+	<div class="flex-left-align">
+		<h3>Mauna Loa CO₂</h3>
+		<p>Mauna Loa CO₂ concentration with 12-month rolling trend</p>
+	</div>
+
+	<DataPane {showDataPane} toggle={toggleDataPane}>
+		<div id="fig-data-pane" class="flex-right-align data-pane">
+			<div id="fig-input" class="data-pane-column"></div>
+		</div>
+	</DataPane>
+
+	<div class="flex-left-align">
+		<Figure {spec} fld="../fluid/co2.fld" />
+	</div>
+</Grid>
